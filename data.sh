251ed9b362d5cb8ae852a910ef57ac15353b63d2
dmateusp <dmateusp@gmail.com>
2016-10-07 10:04:38 +0100

    EAA activity Week 2

---
+++ data.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#Takes user information and displays it
+echo "Type the month you were born in:"
+read month
+echo "Type your favourite color:"
+read color
+echo "Hello $USER, you were born in $month, your favourite color is $color"
