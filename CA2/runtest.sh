3c31c0647db94d27b64104e72243eb79832e94d2
dmateusp <dmateusp@gmail.com>
2016-12-06 11:22:17 +0000

    added results

--- CA2/runtest.sh
+++ CA2/runtest.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Runs with 1, 2, 3,.. to 50 (2 when testing) concurrent users
 echo "C0 N idle"
-for i in {1..10}
+for i in {1..50}
     do
     # loadtest runs during 5 seconds
-    mpstat 3 1 > temp.txt & timeout 5s ./loadtest $i
+    mpstat 4 1 > temp.txt & timeout 5s ./loadtest $i
     cat temp.txt | awk -v N="$i" -v C0="$(cat synthetic.dat | wc -l)" '{
     if($12!="%idle" && $1!="Average:" && $2=="all")
       {
