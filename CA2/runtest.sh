301cf903d4e818dfd8fd883d1f4ce5d7892f613e
dmateusp <dmateusp@gmail.com>
2016-12-06 00:51:57 +0000

    Script ready, still needs to save to file

--- CA2/runtest.sh
+++ CA2/runtest.sh
@@ -4,10 +4,8 @@ echo "C0 N idle"
 for i in {1..10}
     do
     # loadtest runs during 5 seconds
-    timeout 5s ./loadtest $i &
-    # after 4 seconds printing a report on CPU usage
-    sleep 2
-    mpstat | awk -v N="$i" -v C0="$(cat synthetic.dat | wc -l)" '{
+    mpstat 3 1 > temp.txt & timeout 5s ./loadtest $i
+    cat temp.txt | awk -v N="$i" -v C0="$(cat synthetic.dat | wc -l)" '{
     if($12!="%idle" && $1!="Average:" && $2=="all")
       {
          print C0, N, $12
