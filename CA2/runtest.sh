bac551ba364e3e8891b68f4000e53fcc3986be6b
dmateusp <dmateusp@gmail.com>
2016-12-16 20:21:13 +0000

    correcting script to take average

--- CA2/runtest.sh
+++ CA2/runtest.sh
@@ -4,11 +4,12 @@ echo "C0 N idle"
 for i in {1..50}
     do
     # loadtest runs during 5 seconds
-    mpstat 4 1 > temp.txt & timeout 5s ./loadtest $i
+    mpstat 1 10 > temp.txt & timeout 11s ./loadtest $i
     cat temp.txt | awk -v N="$i" -v C0="$(cat synthetic.dat | wc -l)" '{
-    if($12!="%idle" && $1!="Average:" && $2=="all")
+    if($12!="%idle" && $1=="Average:")
       {
          print C0, N, $12
       }
     }'
+    rm temp.txt
 done
